1d1d9d625e636e799356271b76304d301512fd63
Chris Bracewell <=>
2018-02-07 14:59:13 +0000

    Added build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,53 @@
-'use strict';
-const path = require( 'path' )
-const webpack = require( 'webpack' )
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' )
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js"
+  },
+  output: {
+    filename: "bundle.js",
+    filename: "rrjs.bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
   devServer: {
     historyApiFallback: true,
-    contentBase: './dist',
+    contentBase: "./dist",
     hot: true
   },
   module: {
-    rules: [ {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.(html)$/,
-      use: {
-        loader: 'html-loader'
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader"
+        }
       }
-    } ]
+    ]
   },
   resolve: {
-    extensions: [ ".tsx", ".ts", ".js" ]
+    extensions: [".tsx", ".ts", ".js"]
   },
   plugins: [
-    new HtmlWebpackPlugin( {
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
       hash: true,
-      title: 'React',
-      myPageHeader: 'React',
-      template: './src/index.html',
-      filename: './index.html' //relative to root of the application
-    } ),
+      title: "React",
+      myPageHeader: "React",
+      template: "./src/index.html",
+      filename: "./index.html" //relative to root of the application
+    }),
     new webpack.HotModuleReplacementPlugin()
   ]
 };
